6fb2be028aef792d0e18101f5a812597ef00f27d
softgenai <admin@softgen.ai>
2026-04-28 17:15:38 +0000

    feat(auth): wrap app with auth provider in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { Toaster } from "@/components/ui/toaster";
 import { CartProvider } from "@/contexts/CartContext";
+import { AuthProvider } from "@/contexts/AuthContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CartProvider>
-      <Component {...pageProps} />
-      <Toaster />
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </CartProvider>
+    </AuthProvider>
   );
 }
