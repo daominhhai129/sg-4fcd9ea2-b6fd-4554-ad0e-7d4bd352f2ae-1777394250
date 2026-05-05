1e642c1818fce423fbcc9da175902cc278984135
softgenai <admin@softgen.ai>
2026-05-05 09:23:30 +0000

    style(product): shorten desktop button label to "thêm vào giỏ"

--- src/pages/shop/[slug]/product/[id].tsx
+++ src/pages/shop/[slug]/product/[id].tsx
@@ -159,7 +159,7 @@ export default function ProductDetailPage() {
                 onClick={handleAddToCart}
               >
                 <ShoppingCart className="w-5 h-5 mr-2" />
-                Thêm vào giỏ hàng
+                Thêm vào giỏ
               </Button>
               <Button
                 size="lg"
